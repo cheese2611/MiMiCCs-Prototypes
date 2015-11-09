6d03e76ef6ef3084432177acce0b9a8a30a1949f
xesd <sebastian.ulrich@tu-ilmenau.de>
2015-11-09 23:25:40 +0100

    created prototype movement
    Movement in dependency of oculusHeadTracking
    
    2 scripts: playerController (for use without oculus);
    playerControllerOculus (with Oculus)

---
+++ Assets/Scripts/PlayerControllerOculus_MvmtPrototype.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.VR;
+using System.Collections;
+
+public class PlayerControllerOculus : MonoBehaviour
+{
+
+    public float movementSpeed;
+    public float rotationSpeed;
+
+    private Rigidbody rb;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float moveVertical = Input.GetAxis("Vertical");
+        float rotateHorizontal = Input.GetAxis("Horizontal");
+        float oculusRotation = InputTracking.GetLocalRotation(VRNode.Head).eulerAngles.y;
+        float rotation = transform.rotation.eulerAngles.y;
+
+        Vector3 movementForward = new Vector3(moveVertical * Mathf.Sin((rotation + oculusRotation) * Mathf.Deg2Rad), 0.0f, moveVertical * Mathf.Cos((rotation + oculusRotation) * Mathf.Deg2Rad));
+
+        Quaternion turnVertical = transform.rotation * Quaternion.Euler(0.0f, rotateHorizontal * rotationSpeed, 0.0f);
+
+        rb.MovePosition(transform.position + movementForward * movementSpeed);
+        rb.MoveRotation(turnVertical);
+    }
+}
