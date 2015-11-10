4017f5f0c6bc98b7f896c61d2bb4748579d14fb0
cheese2611 <t.vdg@gmx.de>
2015-11-10 16:22:43 +0100

    added Spielzeit

---
+++ Assets/Scripts/InputController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class InputController : MonoBehaviour {
+    public Text uhr;
+
+    void Start () {
+	}
+	
+	void Update () {
+        System.TimeSpan t = System.TimeSpan.FromSeconds(Time.realtimeSinceStartup);
+        uhr.text = string.Format("{0:D2}h:{1:D2}m:{2:D2}s", t.Hours, t.Minutes, t.Seconds);
+	}
+}
