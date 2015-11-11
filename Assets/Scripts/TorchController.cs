ab2e00241aec97d1ba0647c9d4aac52aff946453
mala1436 <martin.langbecker@tu-ilmenau.de>
2015-11-11 18:21:33 +0100

    Created prototype for torch movement
    - single room, single light source
    - torch controlled by right stick movement, click stick to toggle light
    - no camera movement possible
    - options: horizontal/vertical FOV, absolute/relative rotation, invert
    y-axis
    
    - note: FOV does not work with absolute rotation
    - note: rename "torch" to "flashlight" in final product

---
+++ Assets/Scripts/TorchController.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TorchController : MonoBehaviour {
+
+    public Light torch;
+    public float horizontalFOV;
+    public float verticalFOV;
+    public float rotationSpeed;
+    public bool absoluteRotation;
+    public bool invertY;
+
+    private Quaternion originalRotation;
+
+    void Start ()
+    {
+        originalRotation = transform.rotation;
+    }
+
+    void Update()
+    {
+        // rotationY == rotation along y-axis, meaning "horizontal"
+        float rotationY = Input.GetAxis("HorizontalRight");
+        float rotationX = Input.GetAxis("VerticalRight");
+
+        if (invertY)
+        {
+            rotationX = -rotationX;
+        }
+
+        Vector3 rotation = new Vector3(rotationX, rotationY);
+        Vector3 FOV = new Vector3(verticalFOV, horizontalFOV);
+
+        if (absoluteRotation)
+        {
+            transform.Rotate(rotation * rotationSpeed);
+        }
+        else
+        {
+            // multiply components of rotation and FOV vector (Vector3.Scale), transform to Quaternion (Quaternion.Euler)
+            transform.rotation = Quaternion.Slerp(originalRotation, Quaternion.Euler(Vector3.Scale(rotation, FOV)), Time.time * rotationSpeed);
+        }
+
+        // toggle light
+        if (Input.GetButtonDown("Flashlight"))
+        {
+            ToggleTorch();
+        }
+    }
+
+    void ToggleTorch ()
+    {
+        torch.enabled = !torch.enabled;
+    }
+}
