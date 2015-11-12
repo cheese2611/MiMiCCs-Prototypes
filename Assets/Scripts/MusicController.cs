4f2058c8f6ed15e4c94d1202016d112751248ece
xesd <sebastian.ulrich@tu-ilmenau.de>
2015-11-12 16:09:21 +0100

    added behaviour
    - scripts (MusicController, TriggerHandler)
    - Audio Source
    - bool playedMusic (play Music only once)

---
+++ Assets/Scripts/MusicController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicController : MonoBehaviour
+{
+    public AudioClip roomMusic;
+
+    private AudioSource source;
+
+    void Awake()
+    {
+        source = GetComponent<AudioSource>();
+    }
+
+    public void playMusic()
+    {
+        source.PlayOneShot(roomMusic, 1F);
+    }
+}
---
+++ Assets/Scripts/TriggerHandler.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerHandler : MonoBehaviour {
+    
+    public GameObject music;
+
+    private bool musicPlayed = false;
+	
+	void OnTriggerEnter (Collider other) {
+        if (!musicPlayed)
+        {
+            music.GetComponent<MusicController>().playMusic();
+            musicPlayed = true;
+        }
+        
+	}
+}
