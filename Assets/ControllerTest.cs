92b38235f6e4bb54fb5e076ba1c492daf7b79d89
cheese2611 <t.vdg@gmx.de>
2015-11-17 12:27:11 +0100

    added Button images and ControllerTest.cs

---
+++ Assets/ControllerTest.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ControllerTest : MonoBehaviour {
+
+    public Sprite button_0, button_1;
+    
+	void Start () {
+
+	}
+	
+    void Update () {
+        for (int i = 0; i < transform.childCount; ++i)
+        {
+            Transform category = transform.GetChild(i);
+            for (int j = 0; j < category.transform.childCount; ++j)
+            {
+                Transform child = category.transform.GetChild(j);
+                if (category.name == "Buttons")
+                {
+                    if (Input.GetButton(child.name))
+                    {
+                        child.GetComponent<Image>().sprite = button_1;
+                    }
+                    else
+                    {
+                        child.GetComponent<Image>().sprite = button_0;
+                    }
+                }
+                else if (category.name == "Axes")
+                {
+                    child.GetComponent<Slider>().value = Input.GetAxis(child.name);
+                }
+            }
+        }
+    }
+}
