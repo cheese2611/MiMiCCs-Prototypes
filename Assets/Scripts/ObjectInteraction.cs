05a03c4dbce76b9a34d4f842ed55595ae58becc9
mala1436 <martin.langbecker@tu-ilmenau.de>
2015-12-12 02:35:05 +0100

    Implemented prototype for interaction
    - Player Controller script for XBox controller enables basic camera movement on right stick without Oculus Rift
    - Highlight on objects using halos
    - "Interaction range" for player can be set to any distance
    - interaction based on current point of view (position + angle) and distance to collider of target object

---
+++ Assets/Scripts/ObjectInteraction.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectInteraction : MonoBehaviour {
+
+    private Behaviour halo;
+
+    void Start()
+    {
+        halo = (Behaviour)GetComponent("Halo");
+        halo.enabled = false;
+    }
+
+    void Update()
+    {
+        halo.enabled = false;
+    }
+
+    public void Highlight()
+    {
+        halo.enabled = true;
+    }
+}
