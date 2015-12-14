e53ca005eee11dd7e0d074e29d16107e4fce4a6f
cheese2611 <t.vdg@gmx.de>
2015-12-14 22:16:09 +0100

    added more models

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Door : MonoBehaviour {
+
+    Animation anim;
+    bool isInRange = false;
+    bool isOpen = false;
+
+    void Start()
+    {
+        anim = transform.parent.GetChild(1).gameObject.GetComponent<Animation>();
+    }
+
+    void Update()
+    {
+        if (!anim.isPlaying)
+        {
+            if (isInRange != isOpen)
+            {
+                if (isInRange)
+                {
+                    anim.Play("Door_Open");
+                    isOpen = true;
+                }
+                else
+                {
+                    anim.Play("Door_Close");
+                    isOpen = false;
+                }
+            }
+        }
+    }
+
+    void OnTriggerEnter(Collider obj)
+    {
+        isInRange = true;
+    }
+
+    void OnTriggerExit(Collider obj)
+    {
+        isInRange = false;
+    }
+}
