cb4651760aa85418b314fda37104347fb83992f9
cheese2611 <t.vdg@gmx.de>
2015-12-16 23:24:13 +0100

    changed clock controller to show level time

--- Assets/Scripts/ClockController.cs
+++ Assets/Scripts/ClockController.cs
@@ -6,7 +6,7 @@ public class ClockController : MonoBehaviour {
     public Text uhr;
 
 	void Update () {
-        System.TimeSpan t = System.TimeSpan.FromSeconds(Time.realtimeSinceStartup);
+        System.TimeSpan t = System.TimeSpan.FromSeconds(Time.timeSinceLevelLoad);
         uhr.text = string.Format("{0:D2}:{1:D2}:{2:D2}", t.Hours, t.Minutes, t.Seconds);
 	}
 }
