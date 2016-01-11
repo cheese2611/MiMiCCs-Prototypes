9a5ede7a20aba72665ed63520b13b4d8f324a4fb
cheese2611 <t.vdg@gmx.de>
2016-01-11 11:25:27 +0100

    repaired canvas

--- Assets/Scripts/ObjectSpawner.cs
+++ Assets/Scripts/ObjectSpawner.cs
@@ -5,10 +5,13 @@ public class ObjectSpawner : MonoBehaviour {
 
     public GameObject spawnObject;
     public string spawnTag;
+    public string objectName;
 
 	public void Spawn () {
         GameObject[] spawnPoints = GameObject.FindGameObjectsWithTag(spawnTag);
         int spawnPointIndex = Random.Range(0, spawnPoints.Length);
-        Instantiate(spawnObject, spawnPoints[spawnPointIndex].transform.position, spawnPoints[spawnPointIndex].transform.rotation);
+        GameObject spawnedObject = (GameObject)Instantiate(spawnObject, spawnPoints[spawnPointIndex].transform.position, spawnPoints[spawnPointIndex].transform.rotation);
+        spawnedObject.name = objectName;
+        spawnedObject.transform.parent = spawnPoints[spawnPointIndex].transform;
     }
 }
