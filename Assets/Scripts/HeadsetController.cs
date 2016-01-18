0d0f609f6f21e9b170048b5adfb89b729983e8e9
cheese2611 <t.vdg@gmx.de>
2016-01-18 16:25:01 +0100

    added speech and headset + controller

---
+++ Assets/Scripts/HeadsetController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HeadsetController : MonoBehaviour {
+
+    public AudioClip[] clips;
+    AudioSource audioSrc;
+
+    void Start ()
+    {
+        audioSrc = GetComponent<AudioSource>();
+	}
+	
+    public void Test(string clipStr)
+    {
+        foreach (AudioClip clip in clips) {
+            if (clip.name == clipStr)
+            {
+                audioSrc.Stop();
+                audioSrc.clip = clip;
+                audioSrc.Play();
+                return;
+            }
+        }
+        Debug.LogWarning("No clip with name '" + clipStr + "' found");
+    }
+}
