d2a2f741e8e98ed10a66eff0e32c89a366502ceb
cheese2611 <t.vdg@gmx.de>
2016-01-18 13:00:14 +0100

    added electricity animation

--- Assets/Scripts/Electricity.cs
+++ Assets/Scripts/Electricity.cs
@@ -3,12 +3,25 @@ using System.Collections;
 
 public class Electricity : MonoBehaviour
 {
-	
+
+    public float dist = -0.07462686f;
+    public float mag = 6.910449f;
+    public float width = 0.4618358f;
+
 	void Start ()
 	{		
 		Material newMat = GetComponent<Renderer>().material;
 		newMat.SetFloat("_StartSeed", Random.value * 1000);
 		GetComponent<Renderer>().material = newMat;
 	}
+
+    void Update()
+    {
+        Material newMat = GetComponent<Renderer>().material;
+        newMat.SetFloat("_SparkDist", dist);
+        newMat.SetFloat("_SparkMag", mag);
+        newMat.SetFloat("_SparkWidth", width);
+        GetComponent<Renderer>().material = newMat;
+    }
 }
 
