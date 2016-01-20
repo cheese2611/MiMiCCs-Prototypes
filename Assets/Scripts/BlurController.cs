b94ab73eafe2b148208a3678a590009ee6a609ab
cheese2611 <t.vdg@gmx.de>
2016-01-20 13:21:11 +0100

    added eye lids and blur effect

---
+++ Assets/Scripts/BlurController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.ImageEffects;
+
+public class BlurController : MonoBehaviour {
+
+    BlurOptimized blur;
+
+	public void TurnOff() {
+        blur = GetComponent<BlurOptimized>();
+        blur.enabled = true;
+        blur.blurSize = 2.5f;
+        StartCoroutine(Delay(2.0f, Blur0));
+    }
+
+    delegate void Callback();
+    IEnumerator Delay(float delay, Callback cb)
+    {
+        yield return new WaitForSeconds(delay);
+        cb();
+    }
+
+    void Blur0()
+    {
+        blur.blurSize = 1.0f;
+        StartCoroutine(Delay(3.0f, BlurOff));
+    }
+
+    void BlurOff()
+    {
+        blur.enabled = false;
+    }
+}
---
+++ Assets/Scripts/EyeController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class EyeController : MonoBehaviour {
+
+    RawImage lidUpper;
+    RawImage lidLower;
+
+    public float occlusion;
+
+	void Start () {
+        lidUpper = transform.GetChild(0).GetComponent<RawImage>();
+        lidLower = transform.GetChild(1).GetComponent<RawImage>();
+    }
+
+    void Update () {
+	    lidUpper.rectTransform.sizeDelta = new Vector2(0, occlusion * 270);
+        lidLower.rectTransform.sizeDelta = new Vector2(0, occlusion * 270);
+    }
+}
